dc96c7cc95c170ea846dc1a0b0afe485d15007d1
AK059 <75393105+AK059@users.noreply.github.com>
2021-07-15 15:11:15 +1200

    Add project files.

---
+++ BookingSystem/Models/Performances.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Models
+{
+    public class Performances
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ShowId { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+    
+
+}
+    }
+
